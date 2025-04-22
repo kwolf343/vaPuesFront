a7ae7eb4ac8a34a7356551c515174d9124f8b611
kwolf343 <kammonroy@gmail.com>
2025-04-21 22:51:59 -0600

    implementacion de tailwind

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api/v1'  // O la URL de tu backend
+    apiUrl: 'http://localhost:3000/api/v1',
+    //apiUrl: 'https://vhft1zvr-3000.use2.devtunnels.ms/api/v1'
   };
   
